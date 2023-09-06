c67b8615689eb47b7197b4657b7b28ebfb3bb01a
IT21161810 <Mvirajselling@gmail.com>
2023-09-06 23:13:41 +0530

    appjs updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,16 @@ import NavBar from "./pages/NavBar";
 import Registration from "./pages/Registration";
 import Profile from "./pages/Profile";
 
+//viraj imports
+import CardDetails from "./components/CardDetails";
+import Cards from "./components/Cards";
+import CartItem from "./components/CartItem";
+import Header from "./components/Header";
+import AddProduct from "./Onlinestore/AddProduct";
+import OrderItem from "./Onlinestore/OrderItem";
+import UpdateCard from "./Onlinestore/UpdateCard";
+import ViewCart from "./Onlinestore/ViewCart";
+
 function App() {
   const [newCommand, setCommand] = useAtom(command);
   const [newAlanAtom, setAlanAtom] = useAtom(alanAtom);
@@ -39,6 +49,15 @@ function App() {
         <Route path="/" element={<Registration />} />
         <Route path="/login" exact element={<Login />} />
         <Route path="/profile" exact element={<Profile />} />
+
+          {/* viraj routes*/}
+        <Route path="/addnew" element={<AddProduct />}></Route>
+        <Route path="/update/:id" element={<UpdateCard />}></Route>
+        <Route  path='/menu'     element={<Cards/>}/>
+        <Route  path='/cart/:id' element={<CardDetails/>}  />
+        <Route  path='/order/:id' element={<OrderItem/>}  />
+        <Route  path='/view/:id' element={<ViewCart/>}  />
+        <Route path='/cartitem' element={<CartItem/>} />
       </Routes>
     </BrowserRouter>
   );
