a955dc3faa37f40e3c431d66dada384b44de8a71
IT21161810 <Mvirajselling@gmail.com>
2023-09-07 09:32:23 +0530

    navbar updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -45,6 +45,7 @@ function App() {
   return (
     <BrowserRouter>
       <NavBar />
+      {/* <Header/> */}
       <Routes>
         <Route path="/" element={<Registration />} />
         <Route path="/login" exact element={<Login />} />
