44789e4fdb76d41876984bfaa3dad31a5c113c3c
IT21161056 <99235396+IT21161056@users.noreply.github.com>
2023-09-08 22:37:38 +0530

    fixed routes again

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -12,6 +12,10 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
   },
+  phone: {
+    type: String,
+    required: true,
+  },
   password: {
     type: String,
     required: true,
