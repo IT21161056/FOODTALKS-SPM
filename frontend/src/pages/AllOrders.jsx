a2fdc38e210332645b62afcb8836a71d63fc1dd3
Pasindu Perakum <it21160066my.sliit.lk>
2023-09-25 12:03:19 +0530

    new

--- frontend/src/pages/AllOrders.jsx
+++ frontend/src/pages/AllOrders.jsx
@@ -9,12 +9,14 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import IconButton from '@mui/material/IconButton';
+import { Button } from '@mui/material';
 import { Typography } from '@mui/material';
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import EditIcon from '@mui/icons-material/Edit';
 import InputAdornment from '@mui/material/InputAdornment';
 import TextField from '@mui/material/TextField';
 import SearchIcon from '@mui/icons-material/Search';
+import jsPDF from 'jspdf';
 
   function preventDefault(event) {
     event.preventDefault();
@@ -27,7 +29,7 @@ export default function AllOrders () {
     const [deliveryPerson, setDeliveryPerson] = React.useState([]);
     const [isLoading, setIsLoading] = React.useState(false);
     const [employee, setEmployee] = useState([]); 
-     const [searchQuery, setSearchQuery] = useState('');
+    const [searchQuery, setSearchQuery] = useState('');
 
     const employeeId = useParams();
     const employeeName = employee.name;
@@ -35,7 +37,7 @@ export default function AllOrders () {
     const ordersArray = orders;
     console.log(ordersArray);
 
-  {/* Fetch all the orders using useEffect*/}
+    {/* Fetch all the orders using useEffect*/}
 
     useEffect(() => {
       const fetchOrders = async (event) => {
@@ -87,23 +89,85 @@ export default function AllOrders () {
     // Filter orders based on search query
     const filteredOrders = orders.filter((item) => {
       
-      const { customerName, mobileNumber, city } = item;
+      const { customerName, mobileNumber, city, totalAmount } = item;
       const mobileNumberAsString = mobileNumber.toString();
+      const totalAmountAsString = totalAmount.toString();
       const lowerCaseQuery = searchQuery.toLowerCase();
+
       return (
         customerName.toLowerCase().includes(lowerCaseQuery) ||
         mobileNumberAsString.includes(lowerCaseQuery) ||
-        city.toLowerCase().includes(lowerCaseQuery)
+        city.toLowerCase().includes(lowerCaseQuery) || 
+        totalAmountAsString.toLowerCase().includes(lowerCaseQuery)
       );
     });
 
+    // Function to generate the PDF report
+    const generateReport = (orders) => {
+    // Create a new PDF document
+    const doc = new jsPDF();
+  
+    // Define the columns for the table
+    const columns = [
+      { title: 'Customer Name', dataKey: 'customerName' },
+      { title: 'Mobile Number', dataKey: 'mobileNumber' },
+      { title: 'City', dataKey: 'city' },
+      { title: 'Delivery Location', dataKey: 'deliverLocation' },
+      { title: 'Delivery Date', dataKey: 'deliverDate' },
+      { title: 'Delivery Person', dataKey: 'deliveryPerson' },
+      { title: 'Total Amount', dataKey: 'totalAmount' },
+    ];
+  
+    // Create an array of data for the table
+    const data = orders.map((order) => ({
+      customerName: order.customerName,
+      mobileNumber: order.mobileNumber,
+      city: order.city,
+      deliverLocation: order.deliverLocation,
+      deliverDate: order.deliverDate,
+      deliveryPerson: order.deliveryPerson,
+      totalAmount: order.totalAmount,
+    }));
+  
+    // Set the table width and position
+    doc.autoTableSetDefaults({
+      startY: 20,
+      margin: { top: 20},
+      headStyles: { fillColor: [41, 128, 185], textColor: 255 },
+      bodyStyles: { textColor: 0 },
+    });
+  
+    // Add the table to the PDF document
+    doc.autoTable(columns, data);
+  
+    // Save the PDF
+    doc.save('order_report.pdf');
+    };
+
   return (
     <>
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+        <Typography
+          mb={2}
+          sx={{ fontWeight: 600, fontSize: 20, color: "primary", mt: 2 , ml: 2}}
+        >
+          Order List
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          sx={{ marginTop: '10px', marginLeft: 'auto', mr: 2.5 }}
+          onClick={() => {
+            generateReport(filteredOrders); // Call the generateReport function with filteredOrders
+          }}
+        >
+          Generate Report
+        </Button>
+    </div>
     <TextField
         label="Search"
         variant="outlined"
         fullWidth
-        he
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
         InputProps={{
@@ -113,14 +177,14 @@ export default function AllOrders () {
             </InputAdornment>
           ),
         }}
+        sx={{
+          backgroundColor: 'white', 
+          width: "97.5%",
+          margin: 'auto',
+          marginLeft: "10px" 
+        }}
       />
-      <TableContainer  sx={{ maxHeight: "60vh", padding: 1 }}>
-        <Typography
-          mb={2}
-          sx={{ fontWeight: 600, fontSize: 20, color: "primary" }}
-        >
-          Order List
-        </Typography>
+      <TableContainer  sx={{ maxHeight: "70vh", padding: 1 }}>
         <Paper sx={{width: "100%"}}>
           <Table stickyHeader aria-label="sticky table">
 
@@ -134,7 +198,7 @@ export default function AllOrders () {
                 <TableCell sx={{minWidth: 70}} >Deliver Date</TableCell>
                 <TableCell sx={{minWidth: 90}} >Deliver Person</TableCell>
                 <TableCell sx={{minWidth: 80}} >Total Amount</TableCell>
-                <TableCell sx={{minWidth: 70}} >Action</TableCell>
+                <TableCell sx={{minWidth: 60}} >Action</TableCell>
               </TableRow>
 
             </TableHead>
@@ -160,7 +224,8 @@ export default function AllOrders () {
                         <EditIcon />
                         </Link>
                       </IconButton>
-                      <IconButton
+                      <IconButton 
+                        color="error"
                         children={<DeleteForeverIcon/>}
                         onClick={() => deleteOrder(item._id)}
                       />
